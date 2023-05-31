78b8f05544e5c403fce7e072cba226eb625a616c
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-05-31 14:55:08 +0700

    Add debugging

--- src/impl/uri-to-path.ts
+++ src/impl/uri-to-path.ts
@@ -3,6 +3,13 @@ import { fileURLToPath, pathToFileURL } from 'node:url';
 export function uriToPath(uri: string): string {
   const { pathname } = new URL(uri);
 
+  console.debug(
+    uri,
+    pathname,
+    pathname.replace(FIRST_PATH_CHAR_PATTERN, char => `/${char}`),
+    pathToFileURL(process.cwd()),
+  );
+
   return fileURLToPath(
     new URL(
       pathname.replace(FIRST_PATH_CHAR_PATTERN, char => `/${char}`),
