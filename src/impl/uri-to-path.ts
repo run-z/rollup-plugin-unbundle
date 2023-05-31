92bced483c56defda17155d21f24fee64e59e013
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-05-31 14:46:36 +0700

    Fix test under Windows

--- src/impl/uri-to-path.ts
+++ src/impl/uri-to-path.ts
@@ -1,9 +1,14 @@
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath, pathToFileURL } from 'node:url';
 
 export function uriToPath(uri: string): string {
   const { pathname } = new URL(uri);
 
-  return fileURLToPath('file://' + pathname.replace(FIRST_PATH_CHAR_PATTERN, char => `/${char}`));
+  return fileURLToPath(
+    new URL(
+      pathname.replace(FIRST_PATH_CHAR_PATTERN, char => `/${char}`),
+      pathToFileURL(process.cwd()),
+    ).href,
+  );
 }
 
 const FIRST_PATH_CHAR_PATTERN = /^[^/]/;
