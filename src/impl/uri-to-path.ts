91aa4437cd4bf09723071fd8dcc1790f1a820d78
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-05-31 21:19:40 +0700

    Fix URI-to-path conversion

--- src/impl/uri-to-path.ts
+++ src/impl/uri-to-path.ts
@@ -1,6 +1,11 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export function uriToPath(uri: string): string {
+  if (uri.startsWith('file:')) {
+    return fileURLToPath(uri);
+  }
+
   const { pathname } = new URL(uri);
 
   return path.resolve(path.parse(process.cwd()).root, pathname);
